4accb9f93864c1ede3bb9d045dc2adbc8a2b5d34
3asyice <guilhermeabreu294@gmail.com>
2025-06-04 21:04:12 -0300

    update 004.js

--- 004.js
+++ 004.js
@@ -168,72 +168,62 @@ const linkstwentytwentyfour = document.getElementById("linksScripted").innerHTML
 <div class="link-item">
 <a href="https://web.archive.org/web/20250420190604/https://3asyice.github.io/Spck-Test/"/>archived media 004/</a><p class="date">apr 20, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250420185859/https://3asyice.github.io/natal/"/>malditonatal origem/</a><p class="date">apr 20, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250420185859/https://3asyice.github.io/natal/"/>malditonatal origem/</a><p class="date">apr 20, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250419043752/https://www.ocafezinho.com/2024/07/22/exercito-brasileiro-insistente-em-nao-revelar-militares-envolvidos-em-carta-golpista/"/>Carta Golpista EB/</a><p class="date">apr 19, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250419043752/https://www.ocafezinho.com/2024/07/22/exercito-brasileiro-insistente-em-nao-revelar-militares-envolvidos-em-carta-golpista/"/>Carta Golpista EB/</a><p class="date">apr 19, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250418172619/https://www.scielo.br/j/rbh/a/3B49LhDfNKhDKqVPZjvxY5R/"/>Gran Circus Tragedy/</a><p class="date">apr 18, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250418172619/https://www.scielo.br/j/rbh/a/3B49LhDfNKhDKqVPZjvxY5R/"/>Gran Circus Tragedy/</a><p class="date">apr 18, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250405050541/https://ric.com.br/seguranca/acidentes/aluna-ufsm-decidiu-nao-embarcar-na-vespera-de-acidente/"> sorte ou premonição?/</a><p class="date">apr 05, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250405050541/https://ric.com.br/seguranca/acidentes/aluna-ufsm-decidiu-nao-embarcar-na-vespera-de-acidente/"> sorte ou premonição?/</a><p class="date">apr 05, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250405064854/https://www.big-loser.site/Caso/031"/>All Cassetes Fitas/</a><p class="date">apr 05, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250405064854/https://www.big-loser.site/Caso/031"/>All Cassetes Fitas/</a><p class="date">apr 05, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250405045939/https://ric.com.br/seguranca/acidentes/veja-lista-nomes-vitimas-acidente-onibus-ufsm/">bus accident 7 death/</a><p class="date">apr 05, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250405045939/https://ric.com.br/seguranca/acidentes/veja-lista-nomes-vitimas-acidente-onibus-ufsm/">bus accident 7 death/</a><p class="date">apr 05, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250328081437/https://aventurasnahistoria.com.br/noticias/reportagem/perdida-no-oceano-trajetoria-de-bahia-bakari-unica-sobrevivente-de-um-terrivel-acidente-de-aviao.phtml"> Bahia Bakari_survivor/</a><p class="date"> mar 28, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250328081437/https://aventurasnahistoria.com.br/noticias/reportagem/perdida-no-oceano-trajetoria-de-bahia-bakari-unica-sobrevivente-de-um-terrivel-acidente-de-aviao.phtml"> Bahia Bakari_survivor/</a><p class="date"> mar 28, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250328075757/https://www.cnnbrasil.com.br/nacional/sudeste/sp/queda-de-aviao-relembre-cronologia-do-acidente-da-voepass-em-vinhedo-sp/">Voepass 2283/</a> <p class="date"> mar 28, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250328075757/https://www.cnnbrasil.com.br/nacional/sudeste/sp/queda-de-aviao-relembre-cronologia-do-acidente-da-voepass-em-vinhedo-sp/">Voepass 2283/</a> <p class="date"> mar 28, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250328075304/https://cgn.inf.br/noticia/1773188/mae-de-vitima-do-voo-2283-critica-voepass-e-como-se-eles-matassem-nossos-familiares-outra-vez"> critica Voepass/</a><p class="date"> mar 28, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250328075304/https://cgn.inf.br/noticia/1773188/mae-de-vitima-do-voo-2283-critica-voepass-e-como-se-eles-matassem-nossos-familiares-outra-vez"> critica Voepass/</a><p class="date"> mar 28, 2025 </p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250328072720/https://manchetempo.uff.br/?p=2941">242 mortes Boate Kiss/</a> <p class="date"> mar 28, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250328072720/https://manchetempo.uff.br/?p=2941">242 mortes Boate Kiss/</a> <p class="date"> mar 28, 2025 </p>
+<hr></div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250328072137/https://www.bbc.com/portuguese/articles/c89y43gvdl4o">503 dead gran circus/</a> <p class="date"> mar 28, 2025 </p> <hr> </div>
+<a href="https://web.archive.org/web/20250328072137/https://www.bbc.com/portuguese/articles/c89y43gvdl4o">503 dead gran circus/</a> <p class="date"> mar 28, 2025
+</p> <hr> </div>
 <div class="link-item">
-             <a href="https://web.archive.org/web/20250325075854/https://www.big-loser.site/A/Audio/FITA-9,-LADO-A.opus">2:03 Fita 9 Lado a/</a> <p class="date"> mar 25, 2025 </p> <hr> </div> <div class="link-item">
+<a href="https://web.archive.org/web/20250325075854/https://www.big-loser.site/A/Audio/FITA-9,-LADO-A.opus">2:03 Fita 9 Lado a/</a> <p class="date"> mar 25, 2025 </p> <hr> </div> <div class="link-item">
 <a href="https://web.archive.org/web/20250325075425/https://www.big-loser.site/A/Audio/FITA-8,-LADO-B.opus">0:59 Fita 8 Lado b/</a> <p class="date"> mar 25, 2025 </p> <hr> </div>
   <div class="link-item">
-          <a href="https://web.archive.org/web/20250325075007/https://www.big-loser.site/A/Audio/FITA-8,-LADO-A.opus">1:59 Fita 8 Lado a/</a>
-          <p class="date">mar 25, 2025 </p>
-          <hr>
-        </div>
-        <div class="link-item">
-          <a style="text-transform:uppercase;text-shadow:1px 0 0 rgba(18, 255, 0, 1);" href="https://web.archive.org/web/20250320094119/https://www.big-loser.site/Caso/100">BACK TO BLACK/</a>
-          <p class="date">mar 20, 2025 </p>
-          <hr>
-        </div>
-        <div class="link-item">
-          <a href="https://web.archive.org/web/20250319102007/https://www.big-loser.site/Caso/004">004 Archived Media/</a>
-          <p class="date">mar 19, 2025 </p>
-          <hr>
-        </div>
-        <div class="link-item">
-          <a href="https://web.archive.org/web/20250318124659/https://www.big-loser.site/A/Audio/FITA-7,-LADO-B.opus">10:58 Fita 7 Lado B/</a>
-          <p class="date">mar 18, 2025 </p>
-          <hr>
-        </div>
-        <div class="link-item">
-          <a href="https://web.archive.org/web/20250315131320/https://www.big-loser.site/A/Audio/FITA-7,-LADO-A.opus">8:12 Fita 7 Lado A/</a>
-          <p class="date">mar 15, 2025 </p>
-          <hr>
-        </div>
-        <div class="link-item">
-          <a href="https://web.archive.org/web/20250313093351/https://www.big-loser.site/Caso/070">609 butterfly/</a>
-          <p class="date">mar 13, 2025 </p>
-          <hr>
-        </div>
-        <div class="link-item">
-          <a href="https://web.archive.org/web/20250312203737/https://badmgusm.eb.mil.br/index.php/component/content/article?id=2298&">Licenciamento SD EP/</a>
-          <p class="date">mar 12, 2025 </p>
-          <hr>
-        </div>
-        <div class="link-item">
-          <a href="https://web.archive.org/web/20250312131355/https://www.big-loser.site/Caso/005">Camila Dalcol/</a>
-          <p class="date">mar 12, 2025 </p>
-          <hr>
-        </div>
-        <div class="link-item">
-          <a href="https://web.archive.org/web/20250312130354/https://www.big-loser.site/Project_Apocalypse/comandante">everton soares/</a>
+<a href="https://web.archive.org/web/20250325075007/https://www.big-loser.site/A/Audio/FITA-8,-LADO-A.opus">1:59 Fita 8 Lado a/</a>
+  <p class="date">mar 25, 2025</p><hr></div>
+<div class="link-item">
+<a style="text-transform:uppercase;text-shadow:1px 0 0 rgba(18, 255, 0, 1);" href="https://web.archive.org/web/20250320094119/https://www.big-loser.site/Caso/100">BACK TO BLACK/</a>
+<p class="date">mar 20, 2025 </p><hr></div>
+<div class="link-item">
+<a href="https://web.archive.org/web/20250319102007/https://www.big-loser.site/Caso/004">004 Archived Media/</a>
+<p class="date">mar 19, 2025 </p><hr></div>
+<div class="link-item">
+<a href="https://web.archive.org/web/20250318124659/https://www.big-loser.site/A/Audio/FITA-7,-LADO-B.opus">10:58 Fita 7 Lado B/</a>
+<p class="date">mar 18, 2025 </p><hr></div>
+<div class="link-item">
+<a href="https://web.archive.org/web/20250315131320/https://www.big-loser.site/A/Audio/FITA-7,-LADO-A.opus">8:12 Fita 7 Lado A/</a>
+<p class="date">mar 15, 2025 </p>
+<hr>
+</div>
+<div class="link-item">
+<a href="https://web.archive.org/web/20250313093351/https://www.big-loser.site/Caso/070">609 butterfly/
+</a>
+<p class="date">mar 13, 2025 </p><hr></div>
+<div class="link-item">
+<a href="https://web.archive.org/web/20250312203737/https://badmgusm.eb.mil.br/index.php/component/content/article?id=2298&">Licenciamento SD EP/</a>
+<p class="date">mar 12, 2025 </p><hr></div>
+<div class="link-item">
+<a href="https://web.archive.org/web/20250312131355/https://www.big-loser.site/Caso/005">Camila Dalcol/
+</a>
+<p class="date">mar 12, 2025 </p><hr></div>
+<div class="link-item">
+<a href="https://web.archive.org/web/20250312130354/https://www.big-loser.site/Project_Apocalypse/comandante">everton soares/</a>
           <p class="date">mar 12, 2025 </p>
           <hr>
         </div>
