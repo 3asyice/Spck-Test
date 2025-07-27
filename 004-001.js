cfca5bc169de6396def7360a6e3c8a7d71713954
3asyice <guilhermeabreu294@gmail.com>
2025-07-27 20:15:17 -0300

    update 004-001.js

--- 004-001.js
+++ 004-001.js
@@ -19,9 +19,10 @@ const linkstwentytwentyfour = document.getElementById("linksScripted").innerHTML
 
 
 
+<div class="link-item"><a href="https://cdaudiobook.com/13-reasons-why-audiobook/"/>13 r. w. audiobook/</a><p class="date">jul 27, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://famous.ai"/>famous.ai/</a><p class="date">jul 27, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://3asyice.github.io/Spck-Test/quem_sou_eu?.txt"/>quem_sou_eu?.txt/</a><p class="date">jul 27, 2025 </p> <hr> </div>
-<div class="link-item"><a href="https://www.dogpile.com/serp?q=Gay+porn&sc=atYiKf3ziGhg10"/>dogpile.com gayporn/</a><p class="date">jul 27, 2025 </p> <hr> </div>
+<div class="link-item"><a href="https://www.dogpile.com"/>dogpile.com/</a><p class="date">jul 27, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://www.wolframalpha.com"/>Wolframalpha.com/</a><p class="date">jul 27, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://3asyice.github.io/SixOhNine/script/instrucao/pesquisa.html"/>Pesquisa Inteligente/</a><p class="date">jul 25, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://chatgpt.com/"/>chatgpt.com/</a><p class="date">jul 25, 2025 </p> <hr> </div>
