507606683743726683541257fbe1c085ed78379c
3asyice <guilhermeabreu294@gmail.com>
2025-08-14 02:13:52 -0300

    update 004-001.js

--- 004-001.js
+++ 004-001.js
@@ -20,6 +20,7 @@ const linkstwentytwentyfour = document.getElementById("linksScripted").innerHTML
 
 
 
+<div class="link-item"><a href="https://web.archive.org/web/20250814051240if_/https://3asyice.github.io/Tech-Info/script/turma/124/Davi_Pereira.html"/>Davi Pereira/</a><p class="date">ago 14, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://web.archive.org/web/20250814050509if_/https://3asyice.github.io/Tech-Info/script/turma/224/Andre_Oling.html"/>André Oling/</a><p class="date">ago 14, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://web.archive.org/web/20250814045935if_/https://3asyice.github.io/Tech-Info/script/turma/124/Ana_Julia.html"/>Ana Julia Pessoa/</a><p class="date">ago 14, 2025 </p> <hr> </div>
 <div class="link-item"><a style="text-transform:uppercase;" href="https://web.archive.org/web/20250814045202if_/https://3asyice.github.io/Tech-Info/script/turma/424/Guilherme_Goetz.html"/>Guilherme Goetz/</a><p class="date">ago 14, 2025 </p> <hr> </div>
