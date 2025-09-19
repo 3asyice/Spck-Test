d39f15a0d72eef03c978b05f7506b9031e04b93c
3asyice <guilhermeabreu294@gmail.com>
2025-09-19 20:52:19 -0300

    update

--- 004-001.js
+++ 004-001.js
@@ -18,7 +18,7 @@ const linkstwentytwentyfour = document.getElementById("linksScripted").innerHTML
 
 
 
-<div class="link-item"><a href="https://3asyice.github.io/Spck-Test/camila_curriculo.pdf"/>Camila Currículo/</a><p class="date">set 15, 2025 </p> <hr> </div>
+<div class="link-item"><a href="https://3asyice.github.io/Spck-Test/camila_curriculo.pdf"/>Camila Currículo/</a><p class="date">set 19, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/CollectCinequar5/Cabana+do+Inferno+2.MP4.DUB.BaixarSeriesMP4.Com.mp4"/>Cabana do inferno/</a><p class="date">set 15, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/CollectCinequar1/"/>collect cinequar1/</a><p class="date">set 15, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/chavsd8"/>chaves serie/</a><p class="date">set 15, 2025 </p> <hr> </div>
