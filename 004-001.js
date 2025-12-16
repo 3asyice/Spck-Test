c8d24cc65dc9374b1e570003c901089ec78a9a30
3asyice <guilhermeabreu294@gmail.com>
2025-12-15 22:58:07 -0300

    update 004-001.js

--- 004-001.js
+++ 004-001.js
@@ -35,6 +35,8 @@ const linkstwentytwentyfour = document.getElementById("linksScripted").innerHTML
 
 
 
+<div class="link-item"><a href="https://web.archive.org/web/20251216014854id_/https://rad609.bar/Feed.html"/>Rádio 609 Feed</a><p class="date">dec 15, 2025 </p> <hr> </div>
+<div class="link-item"><a href="https://web.archive.org/web/20251216014502id_/https://rad609.bar"/>Rádio 609 </a><p class="date">dec 15, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/bugs_20240315"/>Rabbit Tales </a><p class="date">dec 13, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/looney-tunes-1994-tbs"/>Looney Tunes (1994)</a><p class="date">dec 13, 2025 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/dark_serie_br"/>Dark (2017)</a><p class="date">dec 12, 2025 </p> <hr> </div>
