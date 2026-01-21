056d60a99c6f047631f215303e9a0cd811aef193
3asyice <guilhermeabreu294@gmail.com>
2026-01-21 08:32:07 -0300

    update 004-001.js

--- 004-001.js
+++ 004-001.js
@@ -35,6 +35,8 @@ const linkstwentytwentyfour = document.getElementById("linksScripted").innerHTML
 
 
 
+<div class="link-item"><a href="https://www.tokyvideo.com/br/video/o-predador-1987-2"/>o predador (1987)</a><p class="date">jan 21, 2026 </p> <hr> </div>
+<div class="link-item"><a href="https://www.tokyvideo.com/br/video/o-enigma-de-outro-mundo-1982"/>a coisa (1983)</a><p class="date">jan 21, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://www.tokyvideo.com/br/video/nada-de-novo-no-front-2022-todos"/>nada de novo no front</a><p class="date">jan 21, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://www.tokyvideo.com/br/video/batman-o-cavaleiro-das-trevas-2"/>batman cavaleiro das trevas</a><p class="date">jan 21, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://www.tokyvideo.com/br/video/menina-de-ouro-2004-esporte-drama-vini7xg"/>menina de ouro (2004)</a><p class="date">jan 21, 2026 </p> <hr> </div>
