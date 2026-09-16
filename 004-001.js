a1e0d6e9f5a37764f79b2f95dce49d46f7e92a3f
3asyice <guilhermeabreu294@gmail.com>
2026-09-16 00:36:58 -0300

    update 004-001.js

--- 004-001.js
+++ 004-001.js
@@ -37,6 +37,8 @@ const linkstwentytwentyfour = document.getElementById("linksScripted").innerHTML
 
 
 
+<div class="link-item"><a href="https://archive.org/details/CosmosUmaViagemPessoal"/>cosmos uma viagem pessoal (1980) dublado/</a><p class="date">sep 16, 2026 </p> <hr> </div>
+<div class="link-item"><a href="https://archive.org/details/Freakazoiddublado"/>freakazoid (1995) dublado/</a><p class="date">sep 16, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/homem-aranhaaserieclassica"/>homem aranha - série clássica (1977) dublado/</a><p class="date">sep 16, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/beetlejuice1989dublado"/>os fantasmas se divertem (1989) dublado/</a><p class="date">sep 16, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://archive.org/details/duck-tales-dublado"/>ducktales - dublado/</a><p class="date">sep 16, 2026 </p> <hr> </div>
