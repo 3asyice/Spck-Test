21c38c4376e9fce1b4d4d9f2a7f285291088ab73
3asyice <guilhermeabreu294@gmail.com>
2026-09-18 21:19:15 -0300

    update 004-001.js

--- 004-001.js
+++ 004-001.js
@@ -36,7 +36,8 @@ const linkstwentytwentyfour = document.getElementById("linksScripted").innerHTML
 
 
 
-<div class="link-item"><a href="https://archive.org/details/galinha-pintadinha_20260622"/>galinha pintadinha e sua turma/</a><p class="date">sep 17, 2026 </p> <hr> </div>
+<div class="link-item"><a href="https://archive.org/details/galinha-pintadinha-2-galinha-pintadinha-2-dvd-15-09-2010"/>galinha pintadinha vol 2/</a><p class="date">sep 18, 2026 </p> <hr> </div>
+<div class="link-item"><a href="https://archive.org/details/galinha-pintadinha_20260622"/>galinha pintadinha e sua turma/</a><p class="date">sep 18, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://www.tokyvideo.com/br/video/requiem-para-um-sonho-2000-dublado-1080p"/>requiém para um sonho (2000) dublado/</a><p class="date">sep 16, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://www.tokyvideo.com/br/video/upgrade-atualizacao-2018"/>upgrade atualização (2018) dublado/</a><p class="date">sep 16, 2026 </p> <hr> </div>
 <div class="link-item"><a href="https://www.tokyvideo.com/br/video/se7en-os-sete-crimes-capitais-1995-crime-terror-2h-10m"/>s7ven - os sete crimes capitais (1995) dublado/</a><p class="date">sep 16, 2026 </p> <hr> </div>
